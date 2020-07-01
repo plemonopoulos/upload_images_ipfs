11086021a4fddcc5c523604f5e19e5949179cdbd
plemonopoulos <43816456+plemonopoulos@users.noreply.github.com>
2020-07-01 18:39:32 +0300

    Add files via upload

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,13 @@
+pragma solidity >0.4.24;
+
+contract SimpleStorage {
+  string ipfsHash;
+
+  function set(string memory x) public {
+    ipfsHash = x;
+  }
+
+  function get() public view returns (string memory) {
+    return ipfsHash;
+  }
+}
